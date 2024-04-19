42d3ee96ffe6c157ce6d7352422139b09e647370
Tharun Raj <tharun.2.linux@gmail.com>
2024-04-19 11:30:10 +0530

    created a mobile navigation sidebar. but it's messy :(

--- js/index.js
+++ js/index.js
@@ -3,3 +3,12 @@ const footerYear = document.getElementById("copyrightYear");
 let d = new Date();
 
 footerYear.textContent = d.getFullYear();
+
+const navToggleBtn = document.getElementById("navToggler");
+const sidebar = document.getElementById("sidebarMobileNav");
+
+navToggleBtn.addEventListener("click", () => {
+  console.log("clicked");
+  navToggleBtn.classList.toggle("nav__toggle-btn-active");
+  sidebar.classList.toggle("sidebar__show");
+});
